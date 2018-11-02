13d95f386f7ed9d8554d2d9dab3f032e5876e70b
Yure Mark <yure.mark@gmail.com>
2018-11-02 18:14:11 -0300

    preparando para atualizar registros

--- course/js/main.js
+++ course/js/main.js
@@ -15,7 +15,7 @@ function getTotal(list) {
 function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for(var key in list){
-        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount+'</td><td>'+ formatValue(list[key].value)+'</td><td>Edit  |  Delete</td></tr>'
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount+'</td><td>'+ formatValue(list[key].value)+'</td><td> <buton class="btn btn-default" onclick="setUpdate('+key+');">Edit</buton>Delete</td></tr>'
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -33,7 +33,6 @@ function formatValue(value){
     str = "$" + str;
     return str;
 }
-
 function addData(){
 var desc =  document.getElementById("desc").value;
 var amount =  document.getElementById("amount").value;
@@ -41,9 +40,23 @@ var value =  document.getElementById("value").value;
 
 list.unshift ({"desc":desc, "amount":amount, "value":value});
 setList(list);
-
 }
 
+function setUpdate(id){
+    var obj = list[id];
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("value").value = obj.value;
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+}
 
+function resetForm(){
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
+}
 setList(list);
 console.log(getTotal(list));
